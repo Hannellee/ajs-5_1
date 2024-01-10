dbc5d4843c8dbc284ffaf657b5f475c1377f6429
Anna Bolgova <morfee@mail.ru>
2024-01-10 15:12:21 +0300

    init

---
+++ src/index.js
@@ -0,0 +1,7 @@
+import './js/classman_Character';
+import './js/class_Bowerman';
+import './js/class_Swordsman';
+import './js/class_Magician';
+import './js/class_Daemon';
+import './js/class_Undead';
+import './js/class_Zombie';
